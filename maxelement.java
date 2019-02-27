0bd191c659b3b6ef6a249eda57cec27e16fa06ba
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-02-27 05:32:46 -0800

    Create maxelement.java

---
+++ maxelement.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Solution
+{
+public static void main (String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+n=new int[3];
+for (int i=0;i<=n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<=n;i++)
+{
+max=a[0];
+if a[i]>max;
+{
+max=a[i];
+}
+}
+System.out.println(max);
+}}
+
+
