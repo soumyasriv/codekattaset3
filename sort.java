4686d9b63fb49dd131a03171040cb22d4c2d9e5a
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-03-05 06:24:27 -0800

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,24 @@
+import java.util.Scanner
+class Solution
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[];
+for (int i=0;i<=n;i++)
+{
+a[i]=sc.nextInt();
+}
+for (int i=0;i<=n-1;i++){
+for(int j=0;j<=n-i-1;j++)
+{
+if a[j]>a[j+1]
+{ int temp=a[j+1];
+a[j]=a[i];
+a[i]=temp;
+}}
+for (int i=0;i<=n;i++)
+
+System.ot.println(a[i]+"");
+}}
