8ff9189b2459f1725d76a773ba79fea2d811851b
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-03-11 07:50:27 -0700

    Update median.java

--- median.java
+++ median.java
@@ -1,21 +1,17 @@
 import java.util.Scanner;
-public class Main
+class Main
 {
-	public static void main(String[] args) {
-	    Scanner sc=new Scanner(System.in);
-	    int n=sc.nextInt();
-	    int a[]=new int[5];
-	    int sum=0,median;
-	    for (int i=0;i<=n;i++)
-		{
-		    a[i]=sc.nextInt();
-		}
-		for (int i=0;i<n;i++)
-		{
-		    sum+=a[i];
-		}
-		median=sum/n;
+	public static void main (String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		for(int i=0;i<n;i++)
+			sum+=a[i];
+		int median=sum/n;
 		System.out.println(median);
 	}
 }
-
